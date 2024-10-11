db12accf6069cc4c9bc504f0d065f0d4362d058f
zuikeviciute <zuikeviciute@gmail.com>
2024-10-11 15:47:24 +0100

    html cleanup

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -7,8 +7,8 @@ import TradingViewWidget from "~/components/TradingViewWidget";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    { title: "stock momentum rankings" },
+    { name: "stock momentum rankings", content: "stock momentum rankings" },
   ];
 };
 
@@ -24,24 +24,19 @@ export default function Index() {
       <Navbar />
 
       <div className="flex justify-center mt-14 mx-auto">
-        {/* <aside className="col-span-1 text-[#141414]">watchlist</aside> */}
-
         <div className="max-w-[1140px] flex flex-col md:justify-end">
 
-          {/* Main content container */}
           <div className="flex flex-col sm:flex-col lg:flex-row pb-10 w-full">
-            {/* Stock component */}
+
             <div className="w-full lg:w-1/2 pr-6 mb-6 lg:mb-0">
               {selectedSymbol && <Stock symbol={selectedSymbol} />}
             </div>
 
-            {/* TopPicks component */}
             <div className="w-full lg:w-1/2 flex justify-end text-end">
               <TopPicks onSymbolSelect={handleSymbolSelect} />
             </div>
           </div>
 
-          {/* TradingViewWidget */}
           <div className="h-[440px] w-full sm:h-[440px] sm:w-full lg:h-[620px] lg:w-[1120px]">
             <TradingViewWidget />
           </div>
