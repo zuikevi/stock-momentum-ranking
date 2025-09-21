81809c73e83bdd0841bb9b544f3fda80e8b73063
zuikeviciute <zuikeviciute@gmail.com>
2025-09-21 04:01:23 +0100

    added colourblind and dynamic size options

--- app/components/Stock.tsx
+++ app/components/Stock.tsx
@@ -46,15 +46,14 @@ const Stock: React.FC<selected> = ({ symbol }) => {
                     <p className={`${asset?.watchlist === false ? '' : 'hidden'}`}>Watch</p>
                     <p className={`${asset?.watchlist === true ? '' : 'hidden'}`}>Remove</p>
                 </button>
-
             </div>
 
             <section className={`flex flex-col gap-2`}>
 
                 <div>
                     <p className="text-sm font-semibold pt-2">{assetDetails?.security}</p>
-                    <p className="text-sm font-regular text-wrap">{assetDetails?.subIndustry}</p>
-                    <div className='flex flex-row gap-2 flex-wrap'>
+                    <p className="text-sm font-regular text-wrap pl-4">{assetDetails?.subIndustry}</p>
+                    <div className='flex flex-row gap-2 flex-wrap pl-4'>
                         <p className="text-sm font-regular text-wrap">{assetDetails?.headquarters}</p>
                         <p className="text-sm font-regular text-wrap">{assetDetails?.founded}</p>
                     </div>
@@ -62,7 +61,7 @@ const Stock: React.FC<selected> = ({ symbol }) => {
 
                 <div>
                     <p className="text-sm font-semibold text-wrap">Momentum Score and Price Change</p>
-                    <div className='flex flex-row gap-1 text-left font-semibold'>
+                    <div className='flex flex-row gap-1 text-left font-semibold pl-4'>
                         <p className={`col-span-1 text-sm ${asset!.STM > 0.5 ? 'text-[#94AE32]' : 'text-[#BF503C]'}`}>{asset?.STM}</p>
                         <p className="text-xs pr-10 pt-0.5">STM</p>
                         <p className={`text-sm ${asset!['1D'] > 0 ? 'text-[#94AE32]' : 'text-[#BF503C]'}`}>{asset!['1D']}</p>
@@ -74,7 +73,7 @@ const Stock: React.FC<selected> = ({ symbol }) => {
 
                     </div>
 
-                    <div className='flex flex-row gap-1 text-left font-semibold'>
+                    <div className='flex flex-row gap-1 text-left font-semibold pl-4'>
                         <p className={`col-span-1 text-sm ${asset!.LTM > 0.5 ? 'text-[#94AE32]' : 'text-[#BF503C]'}`}>{asset?.LTM}</p>
                         <p className="text-xs pr-10 pt-0.5">LTM</p>
                         <p className={`text-sm ${asset!['1Y'] > 0 ? 'text-[#94AE32]' : 'text-[#BF503C]'}`}>{asset!['1Y']}</p>
@@ -87,10 +86,17 @@ const Stock: React.FC<selected> = ({ symbol }) => {
                 </div>
             </section>
 
-            <div className='flex flex-wrap flex-row gap-1 pt-2'>
+            <div className='flex flex-wrap flex-row gap-1 pt-4'>
+                    
+                    <div className={`flex flex-row gap-1 shrink  cursor-pointer content-center justify-center min-w-4 font-semibold text-sm`}>
+                        <p className={`border border-1 rounded-full px-2 border-[#CAC8C7] text-[#121212] bg-[#CAC8C7]`}>{asset?.sector}</p>
+                        <p className={`${asset?.watchlist === true ? '' : 'hidden'} border border-1 rounded-full px-2 border-[#CAC8C7] text-[#121212] bg-[#CAC8C7]`}>watched</p>
+                        <p className={`${((asset!.STM >= 0.9) && (asset!.LTM >= 0.9))  ? '' : 'hidden'} border border-1 rounded-full px-2 border-[#79AD18] text-[#79AD18] bg-[#DFE6B6]`}>safe-ish</p>
+                        <p className={`${((asset!.STM <= 0.2) && (asset!.LTM <= 0.2)) ? '' : 'hidden'} border border-1 rounded-full px-2 border-[#BA463B] text-[#BA463B] bg-[#E0B9AC]`}>risky</p>
 
-                <p className={`flex flex-row gap-3 shrink  cursor-pointer content-center justify-center min-w-4 font-semibold text-sm px-2 rounded-full 
-                    border border-1 border-[#DCF367] bg-[#DCF367] text-[#121212]`}>{asset?.sector}</p>
+                        <p className={`${((asset!.STM >= 0.9) && (asset!.LTM <= 0.2)) ? '' : 'hidden'} border border-1 rounded-full px-2 border-[#773CBF] text-[#773CBF] bg-[#BEAAE0]`}>short-term</p>
+                        <p className={`${((asset!.STM <= 0.2) && (asset!.LTM >= 0.9)) ? '' : 'hidden'} border border-1 rounded-full px-2 border-[#773CBF] text-[#773CBF] bg-[#BEAAE0]`}>long-term</p>
+                    </div>
             </div>
         </div>
     );
--- app/components/StocksByTag.tsx
+++ app/components/StocksByTag.tsx
@@ -29,21 +29,24 @@ interface StocksByTagProps {
     tagText: string;
     assetsToDisplay: FilterData[] | undefined;
     view: string;
+    heatmapColour: string;
+    dynamicSize: boolean;
 }
 
 
-const StocksByTag: React.FC<StocksByTagProps> = ({ onSymbolSelect, tagText, assetsToDisplay, view }) => {
+const StocksByTag: React.FC<StocksByTagProps> = ({ onSymbolSelect, tagText, assetsToDisplay, view, heatmapColour, dynamicSize }) => {
 
     const data = useData();
     const [sym, setSym] = useState(' ');
-    if (!data) { return <div>Loading...</div>; }
+    if (!data) { return <div className='font-semibold text-white text-sm'>Loading...</div>; }
     if (!data.filterData) { return <div>Loading...</div>; }
     
-
     const topSTM = (view === 'Both') ? assetsToDisplay?.sort((a, b) => (b.STM + b.LTM) - (a.STM + a.LTM))
         : (view === 'STM') ? assetsToDisplay?.sort((a, b) => (b.STM ?? 0) - (a.STM ?? 0))
         : (view === 'LTM') ? assetsToDisplay?.sort((a, b) => (b.LTM ?? 0) - (a.LTM ?? 0))
         : assetsToDisplay?.sort((a, b) => Number(b[view as keyof typeof b]) - Number(a[view as keyof typeof a]));
+
+    const colourTheme = (heatmapColour === 'blue') ? ['btnBlueT1', 'btnBlueT2', 'btnBlueWhiteT1'] : ['btnGreenT1', 'btnGreenT2', 'btnWhiteT1'];
 
     return (
         <div className={`${tagText === "All Sectors" ? 'lg:w-[760px]' : 'lg:w-[600px]'} flex flex-col place-content-start text-[#FFFFFF] sm:w-fit`}>
@@ -60,25 +63,27 @@ const StocksByTag: React.FC<StocksByTagProps> = ({ onSymbolSelect, tagText, asse
                             hover:border-[#141414] hover:text-[#F2F1EF] hover:bg-[#141414]
 
                         ${(sym === row.symbol) ? 'border-[#141414] text-[#F2F1EF] bg-[#141414]'
-                        : ((row.STM < 0.1 && row.LTM >= 0.9) && view == 'Both') ? 'border-[#38A8C1] bg-[#C8EAF0] text-[#38A8C1]'
-                        : ((row.STM >= 0.9 && row.LTM < 0.1) && view == 'Both') ? 'border-[#773CBF] text-[#773CBF] bg-[#BEAAE0]'
+                        : ((row.STM < 0.2 && row.LTM >= 0.9) && view == 'Both') ? 'border-[#38A8C1] bg-[#C8EAF0] text-[#38A8C1]'
+                        : ((row.STM >= 0.9 && row.LTM < 0.2) && view == 'Both') ? 'border-[#773CBF] text-[#773CBF] bg-[#BEAAE0]'
 
-                        : (((row.STM + row.LTM >= 1.8) && view == 'Both') || (row.STM >= 0.9 && view === 'STM') || (row.LTM >= 0.9 && view === 'LTM')) ? 'btnBlueThemeT1'
-                        : (((row.STM + row.LTM >= 1.6) && view === 'Both') || (row.STM >= 0.8 && view === 'STM') || (row.LTM >= 0.8 && view === 'LTM')) ? 'btnBlueThemeT2'
-                        : ( ((row.STM + row.LTM >= 1.3) && view === 'Both') || (row.STM >= 0.65 && view === 'STM') || (row.LTM >= 0.65 && view === 'LTM')) ? 'btnBlueThemeT3'
-                        : ( ((row.STM + row.LTM >= 1.0) && view === 'Both') || (row.STM >= 0.5 && view === 'STM') || (row.LTM >= 0.5 && view === 'LTM')) ? 'btnBlueThemeWhiteT1'
+                        : (((row.STM + row.LTM >= 1.8) && view == 'Both') || (row.STM >= 0.9 && view === 'STM') || (row.LTM >= 0.9 && view === 'LTM')) ? colourTheme[0]
+                        : (((row.STM + row.LTM >= 1.6) && view === 'Both') || (row.STM >= 0.8 && view === 'STM') || (row.LTM >= 0.8 && view === 'LTM')) ? colourTheme[1]
+                        : ( ((row.STM + row.LTM >= 1.3) && view === 'Both') || (row.STM >= 0.65 && view === 'STM') || (row.LTM >= 0.65 && view === 'LTM')) ? colourTheme[2]
+                        : ( ((row.STM + row.LTM >= 1.0) && view === 'Both') || (row.STM >= 0.5 && view === 'STM') || (row.LTM >= 0.5 && view === 'LTM')) ? colourTheme[2]
                         : ( ((row.STM + row.LTM >= 0.7) && view === 'Both') || (row.STM >= 0.35 && view === 'STM') || (row.LTM >= 0.35 && view === 'LTM')) ? 'btnWhiteT2'
                         : ( ((row.STM + row.LTM >= 0.2) && view === 'Both') || (row.STM >= 0.1 && view === 'STM') || (row.LTM >= 0.1 && view === 'LTM')) ? 'btnRedT2'
                         : ( ((row.STM + row.LTM < 0.2) && view === 'Both') || (row.STM < 0.1 && view === 'STM') || (row.LTM < 0.1 && view === 'LTM')) ? 'btnRedT1'
 
-                        : (Number(row[view as keyof typeof row]) >= 10) ? 'btnBlueThemeT1'
+                        : (Number(row[view as keyof typeof row]) >= 10) ? colourTheme[0]
                         : (Number(row[view as keyof typeof row]) <= -10) ? 'btnRedT1'
-                        : (Number(row[view as keyof typeof row]) >= 3) ? 'btnBlueThemeT2'
+                        : (Number(row[view as keyof typeof row]) >= 3) ? colourTheme[1]
                         : (Number(row[view as keyof typeof row]) <= -3) ? 'btnRedT2'
-                        : (Number(row[view as keyof typeof row]) >= 0) ? 'btnBlueThemeWhiteT1'
+                        : (Number(row[view as keyof typeof row]) >= 0) ? colourTheme[2]
                         : (Number(row[view as keyof typeof row]) < 0) ? 'btnWhiteT2'
                         
                         : 'border-[#CAC8C7] bg-[#E1DFDD]'}
+
+                        ${dynamicSize ? 'flex-auto max-w-24' : ''}
                         `}>
 
                         <p >{row.symbol}</p>
